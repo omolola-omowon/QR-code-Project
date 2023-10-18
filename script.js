546675c544f88ea9fe0e8e89d47b043f89666f09
omolola-omowon <139713666+omolola-omowon@users.noreply.github.com>
2023-10-18 21:11:35 +0100

    new file added for QR scan code

---
+++ script.js
@@ -0,0 +1,100 @@
+
+
+
+/*function myFunction(){
+    let userName = prompt("enter your userName?");
+
+if (userName === "Admin"){
+    
+    let pass = prompt("enter your Password?");
+        if (pass === "TheMaster"){
+            alert("Login Successfully");
+        }
+        
+    
+    else if (pass === '' || pass === null){
+        alert('Wrong Password');
+    }
+    else{
+        alert("Cancelled");
+
+    }
+    
+   
+}
+else if (userName === '' || userName === null){
+    alert('Wrong Username');
+}
+else{
+    alert('Login attempt failed');
+}
+}
+*/
+/*let firstName = [
+    'Allison',
+    'Arthur',
+    'Ana',
+    'Alex',
+    'Arlene',
+    'Alberto',
+    'Barry',
+    'Bertha',
+    'Bill',
+    'Bonnie',
+    'Bret',
+    'Beryl',
+    'Chantal',
+    'Cristobal',
+    'Claudette',
+    'Charley',
+    'Cindy',
+    'Chris'
+    
+    ];
+    let lastName = [
+        'Allison',
+        'Arthur',
+        'Ana',
+        'Alex',
+        'Arlene',
+        'Alberto',
+        'Barry',
+        'Bertha',
+        'Bill',
+        'Bonnie',
+        'Bret',
+        'Beryl',
+        'Chantal',
+        'Cristobal',
+        'Claudette',
+        'Charley',
+        'Cindy',
+        'Chris'
+    ];
+        let fullName = [];
+        for(let i = 0; i < firstName.length; i++){
+            for(let j = 0; j < lastName.length; j++){
+                fullName.push(firstName[i] + " " + lastName[j]);
+                
+                
+            }
+         
+            
+
+        }
+        console.log(fullName);*/
+
+       const newEl = new Date();
+       newEl.setFullYear(2002);
+
+
+
+       console.log(newEl);
+     
+
+      
+
+      
+        
+       
+
